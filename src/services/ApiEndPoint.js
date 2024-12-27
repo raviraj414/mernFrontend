89060aaa8acdecc179acaa5e46223313ae7a5e00
raviraj414 <136338938+raviraj414@users.noreply.github.com>
2024-12-27 11:25:26 +0530

    Update ApiEndPoint.js

--- src/services/ApiEndPoint.js
+++ src/services/ApiEndPoint.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5000',
+    baseURL: 'https://mernbackend-6rie.onrender.com',
     headers: {
         'Content-Type': 'application/json' // Removed extra space
     },
